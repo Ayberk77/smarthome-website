c83349bcebe41f77cf744718b7b1358af9fec748
Duhan Ayberk Seven <c2211062@cankaya.edu.tr>
2026-05-16 18:30:43 +0300

    website update

--- website/client/vite.config.js
+++ website/client/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Custom domain (CNAME: yoursmarthome.app) -> served from "/".
-// If you ever deploy without a custom domain at https://<user>.github.io/smarthome-website/
-// switch this to: base: '/smarthome-website/'
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
 })
